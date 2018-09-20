a87f21973f4d515dc06ae34292ad88fb2e8c4db5
David Hamilton <nova706@gmail.com>
2018-09-20 11:50:33 -0600

    Add CLI support correctly

---
+++ cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var argv = require("minimist")(process.argv.slice(2));
+
+var StaticGFM = require("./index");
+
+if (argv.h || argv.help) {
+    console.log("This is a simple NodeJS app that consumes a .md file and outputs a static HTML file.");
+    console.log("");
+    console.log("-s --src      :   The target md file or directory of md files to convert");
+    console.log("-d --dest     :   The destination file name or directory to export to");
+    console.log("-t --template :   A path to the template.html to use when generating the static HTML");
+    console.log("");
+    process.exit();
+}
+
+var source = (argv._.length > 0) ? argv._[0] : argv.src || argv.s;
+if (source) {
+    var dest = argv.dest || argv.d;
+    var template = argv.template || argv.t;
+    var sgfm = new StaticGFM({dest: dest, template: template});
+    sgfm.convert(source);
+    process.exit();
+} else {
+    console.log("No source to convert. Use -h for help.");
+}
